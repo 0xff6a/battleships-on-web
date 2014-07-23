a49c45bbc304c63cff0bcaab42b4f418e7b024be
Jeremy Fox <foxjerem@gmail.com>
2014-07-23 18:34:12 +0100

    It's working

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -32,12 +32,6 @@ class BattleShips < Sinatra::Base
     redirect "/launch_game/#{session[:player]}" if GAME.start?
     erb :waiting
   end
-    
- #    GAME.add(Player.new(params[:player]))
- #    redirect "/launch_game/#{GAME.current_player.name}" if GAME.player_count == 2
- #    redirect '/full' if GAME.player_count > 2
- #    redirect '/new_game'
-	# end
 
   get '/launch_game/:player' do |player|
     session[:counter] = 0
@@ -47,14 +41,6 @@ class BattleShips < Sinatra::Base
     session[:counter] +=1
     @counter = session[:counter]
     erb :launch_game
-    # until session[:game].current_player.ships.empty?
-    #   erb :launch_game
-    # end
-    # ctr +=1
-    # redirect 'play_game' if ctr == 2
-    #get player to place ships with validation
-    #increment counter
-    #proceed to play_game once both ships have been placed
   end
 
   post '/launch_game/:player/:n' do |player, n|
@@ -71,30 +57,13 @@ class BattleShips < Sinatra::Base
   end
 
   get '/launch_game/:player/waiting' do
+    redirect '/play_game' if GAME.ships_deployed?
     erb :waiting
   end
 
- #  # post '/launch_game' do
- #  #   @game.change_turn
- #  #   redirect '/launch_game' 
- #  # end
-
- #  post '/play_game' do
- #    puts params
- #    "Hi"
- #    #Loop
- #    #Prompt the player
- #    #Display their tracking grid
- #    #Ask for coordinate (radio buttons?)
- #    #Return cell message
- #    #Attack cell if valid
- #    #end game if victory declared
- #    #change player
- #  end
-
- #  get '/play_game' do
-
- #  end
+  get '/play_game' do
+    erb :play_game
+  end
 
 
   # start the server if ruby file executed directly
